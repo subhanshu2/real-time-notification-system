bb50b2470f3c2951271a6261999632c465d1dea0
Subhanshu <subhanshu@192.168.1.4>
2026-02-13 19:53:17 +0530

    Moving the root directory of prisma in order to support PrismaClient

---
+++ backend/src/config/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["query", "warn", "error"],
+  });
+  
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
